6695ed5d8c9f586ec4ba6bad0855adeca5550106
Mathieu Paillart <mathieu.paillart@etu.unice.fr>
2020-01-28 08:28:23 +0100

    account (REST route implemented to get all transactions) profile (added rest route to get all profiles) dump (print is done) TODO : replace transactionId (it's not that value)

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM alexisgra/falcon-alpine-python-3.7.4:latest
 
 ADD dump.py ./
 
-CMD ["sh", "-c", "gunicorn -b 0.0.0.0:8084 'dump:setup_dump(\"http://account:8081\",\"http://profile:8083\")'"]
+CMD ["sh", "-c", "gunicorn -b 0.0.0.0:8084 'dump:setup_dump(\"http://account:8082\",\"http://profile:8083\")'"]
 
 
--- dump.py
+++ dump.py
@@ -1,6 +1,4 @@
 import falcon
-import json
-import os
 import requests
 
 
@@ -9,12 +7,48 @@ class DumpInfo:
     profile_url = None
 
 
+def getTransactions(transactions, accountId):
+    print(accountId)
+    transactionsFiltered = []
+    for transaction in transactions:
+        if transaction['source'] == accountId or transaction['receiver'] == accountId:
+            transactionsFiltered.append(transaction)
+    return transactionsFiltered
+
+
+def getAccounts(accounts, email):
+    accountsFiltered = []
+    for account in accounts:
+        if account['owner']['email'] == email:
+            accountsFiltered.append(account)
+    return accountsFiltered
+
+
 # port : 8083
 class DumpController(object):
     def on_get(self, req, resp):
-        x =requests.get(DumpInfo.account_url + '/accounts/SAVINGS/' + "accounts")
-        print(x)
-        resp.body = json.dumps(x)
+        result = ''
+        savingsAccount = requests.get(DumpInfo.account_url + '/accounts/SAVINGS/' + 'accounts')
+        checksAccount = requests.get(DumpInfo.account_url + '/accounts/CHECK/' + 'accounts')
+        transactions = requests.get(DumpInfo.account_url + '/transactions')
+        profiles = requests.get(DumpInfo.profile_url + '/profiles')
+        for profile in profiles.json():
+            accountsFiltered = getAccounts(savingsAccount.json(), profile['_id']) + getAccounts(checksAccount.json(),
+                                                                                                profile['_id'])
+            result += profile['_id'] + '   ' + str(len(accountsFiltered)) + ' Accounts\n'
+            for account in accountsFiltered:
+                result += ' accountId : ' + account['accountId'] + '\n'
+                transactionsFiltered = getTransactions(transactions.json(), account['accountId'])
+                result += '   Money : ' + str(account['money']) + '   transaction : ' + str(
+                    len(transactionsFiltered)) + '\n\n'
+                for transaction in transactionsFiltered:
+                    result += '     transactionId : '
+                    if transaction['source'] == account['accountId']:
+                        result += transaction['source'] + ' - ' + str(transaction['amount']) + '\n'
+                    elif transaction['receiver'] == account['accountId']:
+                        result += transaction['receiver'] + ' + ' + str(transaction['amount']) + '\n'
+                result += '\n'
+        resp.body = result
         resp.status = falcon.HTTP_200
         return resp
 
@@ -23,7 +57,6 @@ def setup_dump(account_url, profile_url):
     if account_url is not None and profile_url is not None:
         DumpInfo.account_url = account_url
         DumpInfo.profile_url = profile_url
-
         print(DumpInfo.account_url)
         print(DumpInfo.profile_url)
     app = falcon.API()
